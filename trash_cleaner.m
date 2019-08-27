ae90b10cdbb5ca74cbe6d7f517fc068675b34569
Vitaly Chernushev <chernush.vitaly@yandex.ru>
2019-08-27 10:33:28 +0300

    corrected for new tables

--- trash_cleaner.m
+++ trash_cleaner.m
@@ -1,16 +1,12 @@
 function input = trash_cleaner(input)
 %cleaning xls file
-input(56) = [];
-input(55) = [];
-input(45) = [];
-input(44) = [];
-input(35) = [];
+input(63) = [];
+input(54) = [];
+input(43) = [];
 input(34) = [];
-input(22) = [];
 input(21) = [];
-input(14) = [];
 input(13) = [];
-input(1) = [];
+
 
 end
 
