795f5bf6bd58b873c3c059586e946ccb4a7bec6c
Vitaly Chernushev <chernush.vitaly@yandex.ru>
2019-09-16 16:25:29 +0300

    added phenomenons edit in xlsx and autoedit in output

--- xlsx_mat.m
+++ xlsx_mat.m
@@ -1,17 +1,20 @@
 clc
 clear
 
-% [~,night_table,~] = xlsread([pwd, '/new_data_sample.xlsx'], 1,'A2:AF67');
-% [~,day_table,~] = xlsread([pwd, '/new_data_sample.xlsx'], 1,'AH2:BM67');
-% night_table(3,:) = [];
-% day_table(3,:) = [];
-% night_table(:,8) = [];
-% night_table(:,5) = [];
-% day_table(:,8) = [];
-% day_table(:,5) = [];
-% save('day.mat', 'day_table');
-% save('night.mat', 'night_table');
-[~,night_assessment,~] = xlsread([pwd, '/bin/оценка_прогнозирования.xlsx'], 1,'A2:AH67');
-[~,day_assessment,~] = xlsread([pwd, '/bin/оценка_прогнозирования.xlsx'], 2,'A2:AH67');
-save('night_assessment.mat', 'night_assessment');
-save('day_assessment.mat', 'day_assessment');
+[~,night_table,~] = xlsread([pwd, '/образец.xlsx'], 1,'A2:AF67');
+[~,day_table,~] = xlsread([pwd, '/образец.xlsx'], 1,'AH2:BM67');
+night_table(3,:) = [];
+day_table(3,:) = [];
+night_table(:,8) = [];
+night_table(:,5) = [];
+day_table(:,8) = [];
+day_table(:,5) = [];
+day_table(2,15) = day_table(2,27);
+night_table(2,15) = night_table(2,27);
+
+save('day.mat', 'day_table');
+save('night.mat', 'night_table');
+% [~,night_assessment,~] = xlsread([pwd, '/bin/оценка_прогнозирования.xlsx'], 1,'A2:AH67');
+% [~,day_assessment,~] = xlsread([pwd, '/bin/оценка_прогнозирования.xlsx'], 2,'A2:AH67');
+% save('night_assessment.mat', 'night_assessment');
+% save('day_assessment.mat', 'day_assessment');
