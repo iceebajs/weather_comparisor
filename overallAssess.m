abf47abe68017343d163a3346dad2d65fdd8113b
Vitaly Chernushev <chernush.vitaly@yandex.ru>
2020-01-16 16:48:19 +0300

    improved error logger and some bug fixes

--- overallAssess.m
+++ overallAssess.m
@@ -1,33 +1,30 @@
 function out = overallAssess(t,p, assess)
 out = zeros(size(assess));
-try
-    for n = 1:length(assess)
-        if ~isnan(assess(n)) && isnumeric(p{n}) && isnumeric(t{n})
-            z = (p{n}+t{n}+assess(n))/3;
-            z = z - floor(z);
-            
-            if z < 0.5
-                out(n) = floor((p{n}+t{n}+assess(n))/3);
-            else
-                out(n) = ceil((p{n}+t{n}+assess(n))/3);
-            end
-        elseif isnumeric(p{n}) && isnumeric(t{n})
-            z = (p{n}+t{n})/2;
-            z = z - floor(z);
-            
-            if z < 0.5
-                out(n) = floor((p{n}+t{n})/2);
-            else
-                out(n) = ceil((p{n}+t{n})/2);
-            end
-        else
-            out(n) = NaN;
-        end
-    end
+
+for n = 1:length(assess)
+    if ~isnan(assess(n)) && isnumeric(p{n}) && isnumeric(t{n})
+        z = (p{n}+t{n}+assess(n))/3;
+        z = z - floor(z);
         
-catch
-    out{1} = 'Критическая ошибка расчета';
+        if z < 0.5
+            out(n) = floor((p{n}+t{n}+assess(n))/3);
+        else
+            out(n) = ceil((p{n}+t{n}+assess(n))/3);
+        end
+    elseif isnumeric(p{n}) && isnumeric(t{n})
+        z = (p{n}+t{n})/2;
+        z = z - floor(z);
+        
+        if z < 0.5
+            out(n) = floor((p{n}+t{n})/2);
+        else
+            out(n) = ceil((p{n}+t{n})/2);
+        end
+    else
+        out(n) = NaN;
+    end
 end
+
 
 end
 
--- temperature.m
+++ temperature.m
@@ -1,58 +1,54 @@
 function assessment_t = temperature(fact_t,forecast_t)
 assessment_t{length(fact_t),1} = {};
-try
-    for n  = 1:length(forecast_t)
-        if ~isnan(sum(forecast_t{n})) && ~isnumeric(forecast_t{n})
-            forecast_t{n} = str2num(forecast_t{n});
-        end
+for n  = 1:length(forecast_t)
+    if ~isnan(sum(forecast_t{n})) && ~isnumeric(forecast_t{n})
+        forecast_t{n} = str2num(strrep(forecast_t{n},',','.'));
     end
-    
-    for n = 1:length(fact_t)
-        if ~isnan(sum(fact_t{n})) && ~isnumeric(fact_t{n})
-            fact_t{n} = str2num(fact_t{n});
-        end
+end
+
+for n = 1:length(fact_t)
+    if ~isnan(sum(fact_t{n})) && ~isnumeric(fact_t{n})
+        fact_t{n} = str2num(strrep(fact_t{n},',','.'));
     end
-    
-    for n  = 1:length(fact_t)
-        temp = NaN;
-        if ~isnan(forecast_t{n})
-            if length(forecast_t{n}) < 2
+end
+
+for n  = 1:length(fact_t)
+    temp = NaN;
+    if ~isnan(forecast_t{n})
+        if length(forecast_t{n}) < 2
+            assessment_t{n} = 'Ошибка';
+            continue;
+        else
+            if ~isempty(fact_t{n}) && ~isempty(forecast_t{n})
+                if forecast_t{n}(1) > forecast_t{n}(2)
+                    tmp = forecast_t{n}(1);
+                    forecast_t{n}(1) = forecast_t{n}(2);
+                    forecast_t{n}(2) = tmp;
+                end
+                if fact_t{n} >= forecast_t{n}(1) && fact_t{n} <= forecast_t{n}(2)
+                    assessment_t{n} = 100;
+                    continue;
+                elseif fact_t{n} <= forecast_t{n}(1)
+                    temp = abs(fact_t{n}-forecast_t{n}(1));
+                elseif fact_t{n} >= forecast_t{n}(2)
+                    temp = abs(fact_t{n}-forecast_t{n}(2));
+                end
+            else
                 assessment_t{n} = 'Ошибка';
                 continue;
-            else
-                if ~isempty(fact_t{n}) && ~isempty(forecast_t{n})    
-                    if forecast_t{n}(1) > forecast_t{n}(2)
-                        tmp = forecast_t{n}(1);
-                        forecast_t{n}(1) = forecast_t{n}(2);
-                        forecast_t{n}(2) = tmp;
-                    end
-                    if fact_t{n} >= forecast_t{n}(1) && fact_t{n} <= forecast_t{n}(2)
-                        assessment_t{n} = 100;
-                        continue;
-                    elseif fact_t{n} <= forecast_t{n}(1)
-                        temp = abs(fact_t{n}-forecast_t{n}(1));
-                    elseif fact_t{n} >= forecast_t{n}(2)
-                        temp = abs(fact_t{n}-forecast_t{n}(2));
-                    end
-                else
-                    assessment_t{n} = 'Ошибка';
-                    continue;
-                end
             end
         end
-        
-        if temp <= 2
-            assessment_t{n} = 100;
-        elseif temp <= 3
-            assessment_t{n} = 50;
-        elseif isnan(temp)
-            assessment_t{n} = '';
-        else
-            assessment_t{n} = 0;
-        end
     end
-catch
-    assessment_t{n} = 'Крит.ошибка расчета данной точки';
+    
+    if temp <= 2
+        assessment_t{n} = 100;
+    elseif temp <= 3
+        assessment_t{n} = 50;
+    elseif isnan(temp)
+        assessment_t{n} = '';
+    else
+        assessment_t{n} = 0;
+    end
 end
 
 end
